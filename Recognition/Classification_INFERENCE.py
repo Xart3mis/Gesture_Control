f6f48be9c057694143a4acfef5ef88e46d26f967
Xart3mis <ihavemanywaifus@protonmail.com>
2022-06-10 01:43:50 +0000

    change algorithm from knn and sax to BOSSVS algorithm and fix the way data was being structured

--- Recognition/Classification_INFERENCE.py
+++ Recognition/Classification_INFERENCE.py
@@ -2,13 +2,16 @@ import serial
 import pickle
 import numpy as np
 from time import sleep
-from tslearn import utils
+from scipy.interpolate import interp1d
 
 print("Loading Model and Labels")
 pickle_f = pickle.load(open("/home/lethargic/Documents/PicoMPU9250/Models/Classsifier.pickle", "rb"))
 
 labels = pickle_f[1]
-clf = pickle_f[0]
+Tnew = pickle_f[0]
+clf = pickle_f[2]
+
+MIN_GESTURE_LEN = 5
 
 
 class SerialIMU:
@@ -17,28 +20,37 @@ class SerialIMU:
         self.gesture = []
 
     def update(self) -> None:
+        h, p, r, ax, ay, az, mx, my, mz = [], [], [], [], [], [], [], [], []
         while self.pyb.in_waiting > 8:
             line = self.pyb.readline().decode("utf-8").strip()
-            if "Traceback" in line:
-                print("something went wrong on the serial device.")
-                return
+            sleep(0.06)
             print("reading gesture... ", end="      \r")
             # print(line)
             line_arr = line.split(",")
-            self.gesture.append(
-                [
-                    float(line_arr[0]),
-                    float(line_arr[1]),
-                    float(line_arr[2]),
-                    float(line_arr[3]) * 100,
-                    float(line_arr[4]) * 100,
-                    float(line_arr[5]) * 100,
-                    float(line_arr[6]) * 100,
-                    float(line_arr[7]) * 100,
-                    float(line_arr[8]) * 100,
-                ]
-            )
-            sleep(0.06)
+            try:
+                h.append(float(line_arr[0]))
+                p.append(float(line_arr[1]))
+                r.append(float(line_arr[2]))
+                ax.append(float(line_arr[3]))
+                ay.append(float(line_arr[4]))
+                az.append(float(line_arr[5]))
+                mx.append(float(line_arr[6]))
+                my.append(float(line_arr[7]))
+                mz.append(float(line_arr[8]))
+
+            except ValueError as e:
+                if "string to float" in str(e).lower():
+                    print("something went wrong on embedded side")
+                    return
+                print(line, e)
+        if len(h) > MIN_GESTURE_LEN:
+            print("yadin omy")
+            self.gesture.append([h, p, r, ax, ay, az])
+
+            for i, v in enumerate(self.gesture):
+                Told = np.arange(0, len(v[0]))
+                F = interp1d(Told, v, fill_value="extrapolate")
+                self.gesture[i] = F(Tnew)
 
 
 if __name__ == "__main__":
@@ -48,11 +60,9 @@ if __name__ == "__main__":
         simu.update()
 
         if len(simu.gesture) > 0:
-            if len(simu.gesture) > 10:
-                gesture = np.array([simu.gesture])
-                if len(gesture.shape) == 3:
-                    if gesture.shape[2] == 9:
-                        print("Recognizing gesture...")
-                        print(labels[clf.predict(utils.to_time_series(gesture))[0]])
+            if len(simu.gesture[0]) > MIN_GESTURE_LEN:
+                print("WHAT")
+                gesture = np.asarray(simu.gesture)
+                print(labels[clf.predict(gesture)[0]])
 
-        simu.gesture.clear()
+            simu.gesture.clear()
--- Recognition/Classification_TRAIN.py
+++ Recognition/Classification_TRAIN.py
@@ -2,12 +2,11 @@ import os
 import json
 import pickle
 import numpy as np
-from rdp import rdp
 import pandas as pd
-from scipy import stats
-from tslearn import utils
-from tslearn import neighbors
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from pyts.classification import BOSSVS
+from pyts.multivariate.classification import MultivariateClassifier
 
 path = "/home/lethargic/Documents/PicoMPU9250/data"
 
@@ -21,11 +20,12 @@ labels = {}
 for i, v in enumerate(list(next(os.walk(path))[1])):
     labels[i] = v
 
+
 for i in [path + "/" + list(next(os.walk(path))[1])[i] for i in range(len(list(next(os.walk(path))[1])))]:
     for j in os.walk(i):
         for k in j[2]:
             data = pd.read_csv(i + "/" + k)
-            __x = []
+            _h, _r, _p, _ax, _ay, _az, _mx, _my, _mz = [], [], [], [], [], [], [], [], []
             for h, r, p, ax, ay, az, mx, my, mz in zip(
                 data.heading.tolist(),
                 data.roll.tolist(),
@@ -37,33 +37,44 @@ for i in [path + "/" + list(next(os.walk(path))[1])[i] for i in range(len(list(n
                 data.my.tolist(),
                 data.mz.tolist(),
             ):
-                __x.append([h, r, p, ax * 100, ay * 100, az * 100, mx * 100, my * 100, mz * 100])
+                _h.append(h)
+                _r.append(r)
+                _p.append(p)
+                _ax.append(ax)
+                _ay.append(ay)
+                _az.append(az)
+                _mx.append(mx)
+                _my.append(my)
+                _mz.append(mz)
+
             # y.append()
-            x.append(__x)
+            x.append([_h, _r, _p, _ax, _ay, _az])
             for l in labels.items():
                 if l[1] in k:
                     y.append(l[0])
 
-# y = np.array(y)
-# print(y.shape)
-# for i, v in enumerate(x):
-#     x[i] = rdp(v, epsilon=0.4)
-time_series = utils.to_time_series_dataset(x)
-clf = neighbors.KNeighborsTimeSeriesClassifier(metric="sax", n_neighbors=5)
-print(len(y))
-print(len(x))
+clf = MultivariateClassifier(BOSSVS())
 
+Tnew = np.arange(0, len(max(x, key=lambda s: len(s[0]))[0]))
 
-def plot_dataset():
-    for i in time_series:
-        plt.plot(np.linspace(0, 100000, num=len(i)), i, marker="o")
-    plt.show()
+for i, v in enumerate(x):
+    Told = np.arange(0, len(v[0]))
 
+    F = interp1d(Told, v, fill_value="extrapolate")
+    x[i] = F(Tnew)
+
+x = np.asarray(x)
+
+plt.show()
+clf.fit(x, y)
 
 if __name__ == "__main__":
-    clf.fit(time_series, y)
+    print(clf.score(x, y))
+
     data = pd.read_csv("/home/lethargic/Documents/PicoMPU9250/predict_test/test.csv")
-    testx = []
+    test_x = []
+    _h, _r, _p, _ax, _ay, _az, _mx, _my, _mz = [], [], [], [], [], [], [], [], []
+
     for h, r, p, ax, ay, az, mx, my, mz in zip(
         data.heading.tolist(),
         data.roll.tolist(),
@@ -75,9 +86,24 @@ if __name__ == "__main__":
         data.my.tolist(),
         data.mz.tolist(),
     ):
-        testx.append([h, r, p, ax * 100, ay * 100, az * 100, mx * 100, my * 100, mz * 100])
+        _h.append(h)
+        _r.append(r)
+        _p.append(p)
+        _ax.append(ax)
+        _ay.append(ay)
+        _az.append(az)
+        _mx.append(mx)
+        _my.append(my)
+        _mz.append(mz)
+    test_x.append([_h, _r, _p, _ax, _ay, _az])
 
-    time_series_testx = utils.to_time_series([testx])
-    print(labels[clf.predict(time_series_testx)[0]])
-    pickle.dump([clf, labels], open("/home/lethargic/Documents/PicoMPU9250/Models/Classsifier.pickle", "wb"))
-    plot_dataset()
+    for i, v in enumerate(test_x):
+        Told = np.arange(0, len(v[0]))
+        F = interp1d(Told, v, fill_value="extrapolate")
+        test_x[i] = F(Tnew)
+
+    test_x = np.asarray(test_x)
+    print(test_x.shape)
+
+    print(labels[clf.predict(test_x)[0]])
+    pickle.dump([Tnew, labels, clf], open("/home/lethargic/Documents/PicoMPU9250/Models/Classsifier.pickle", "wb"))
