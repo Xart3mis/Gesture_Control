e061980b29ea1a0d86c1a4f1e87ea102224e24a8
Xart3mis <Xart3misX@protonmail.com>
2022-06-18 01:52:48 +0200

    diff path for laptop

--- tools/mpr-run.sh
+++ tools/mpr-run.sh
@@ -1,4 +1,4 @@
-cd ~/Documents/PicoMPU9250
-python tools/pyboard.py -d /dev/ttyACM0 -f cp src/main.py :
-python tools/pyboard.py -d /dev/ttyACM0 -f cp src/lib/*.py :lib/
-python tools/pyboard.py -d /dev/ttyACM0 $1
+cd ~/Documents/Gesture_Control
+python3 tools/pyboard.py -d /dev/ttyACM0 -f cp src/main.py :
+# python3 tools/pyboard.py -d /dev/ttyACM0 -f cp src/lib/*.py :lib/
+python3 tools/pyboard.py -d /dev/ttyACM0 $1
